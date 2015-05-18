dea0adb62320f38dd11b815177ddeb982893f0ed
anand raman <araman@gmail.com>
2015-05-18 14:22:12 +0530

    [ar] the finders work in parallel

--- src/main/java/engine/impl/RamlDiffEngineImpl.java
+++ src/main/java/engine/impl/RamlDiffEngineImpl.java
@@ -13,7 +13,6 @@ import engine.Finder;
 import engine.RamlDiffEngine;
 import engine.impl.finders.FindActionsWithDifferingResponseSchemas;
 import engine.impl.finders.FindActionsWithUpdatedQueryParameters;
-import engine.impl.finders.FindActionsWithUpdatedResponseSchema;
 import engine.impl.finders.FindActionsWithUpdatedResponseStatusCode;
 import engine.impl.finders.FindActionsWithUpdatedTraits;
 import engine.impl.finders.FindNewActions;
@@ -21,17 +20,13 @@ import engine.impl.finders.FindRemovedActions;
 
 public class RamlDiffEngineImpl implements RamlDiffEngine {
 
-  protected List<Finder> finders = Arrays.asList(
-      new FindNewActions(), 
-      new FindRemovedActions(),
-      new FindActionsWithUpdatedQueryParameters(),
-      new FindActionsWithUpdatedTraits(),
-      new FindActionsWithUpdatedResponseStatusCode(),
-      new FindActionsWithDifferingResponseSchemas());
+  protected List<Finder> finders = Arrays.asList(new FindNewActions(), new FindRemovedActions(),
+      new FindActionsWithUpdatedQueryParameters(), new FindActionsWithUpdatedTraits(),
+      new FindActionsWithUpdatedResponseStatusCode(), new FindActionsWithDifferingResponseSchemas());
 
   public List<ActionDiff> findDifferences(Map<ActionId, Action> newActions, Map<ActionId, Action> oldActions) {
 
-    List<ActionDiff> allDifferences = finders.stream().flatMap(finder -> {
+    List<ActionDiff> allDifferences = finders.stream().parallel().flatMap(finder -> {
       return finder.diff(newActions, oldActions).stream();
     }).collect(Collectors.toList());
 
